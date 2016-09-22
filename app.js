3fc0d03d11cbfc2d94fdde4ae30898f985f421f5
RajBhavani <rbhavani@outlook.com>
2016-09-22 14:25:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ userSearch = ""
     //Train Station
         $.getJSON("https://api.foursquare.com/v2/venues/search?v=20131016&near="+userSearch+"&intent=browse&radius=8000&categoryId=4bf58dd8d48988d129951735&client_id=1FK5WICU4PWL15FW2MOWLXGFE3IXN5ILH2R1NYLWJ1DCW2ZK&client_secret=BQIULM0LQSCHYXKKEEEF3FRRANDBQQG0ML5KY2214M3XS51A", function(data){
         var myResponse = (data.response);
-        //console.log(myResponse);
+        console.log(myResponse);
        //TrainAvaliability
         $.getJSON("https://api.foursquare.com/v2/venues/search?v=20131016&near="+userSearch+"&intent=browse&radius=8000&categoryId=4bf58dd8d48988d12a951735&client_id=1FK5WICU4PWL15FW2MOWLXGFE3IXN5ILH2R1NYLWJ1DCW2ZK&client_secret=BQIULM0LQSCHYXKKEEEF3FRRANDBQQG0ML5KY2214M3XS51A", function(train){
         var Response = (train.response);
